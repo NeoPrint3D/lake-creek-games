bf89dd29c6ce62e5a387103c1f3939846b85aaa7
Drew <drew@ronsman.com>
2022-03-10 14:11:44 -0600

    first workflow

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ import cryptojs from "crypto-js";
 
 import axios from "axios";
 
+const api_key = process.env.API_KEY;
+const hash_key = process.env.HASH_KEY;
 
+console.log(api_key, hash_key);
+
+function encrypt(hash, key) {
+    try {
+        let bytes = cryptojs.AES.encrypt(JSON.stringify(hash), key);
+        return bytes.toString();
+    } catch (error) {
+        return null;
+    }
+}
+
+
+
+axios.post('https://lakecreekgames-neoprint3d.vercel.app/cronIndex', {
+    api_key: encrypt(
+        api_key, hash_key
+    ),
+})
